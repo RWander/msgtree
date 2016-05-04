468bb9997304ea405dd66eefdb2e7d3742947e7b
Roman Korneyev <roman.korneyev@gmail.com>
2016-05-04 22:05:11 +0400

    data generator for tests

--- test/spec/routes.spec.js
+++ test/spec/routes.spec.js
@@ -1,19 +1,35 @@
 'use strict';
 
 /*eslint-env jasmine */
+/* eslint-disable no-console */
 
 describe('Athlete routes', function()  {
+  const mongoose = require('mongoose');
   const request = require('supertest');
   const server = require('../../app');
+  const generator = require('../generator');
 
   let app;
 
   beforeAll(function(done)  {
+    // run server
     app = server.app;
-    server.start(done);
+    server.start(() => {
+      // drop test database
+      mongoose.connection.db.dropDatabase(() => {
+        console.log('Test database is dropped.');
+
+        // create test database
+        generator((err) => {
+          if (err) throw err;
+          console.log('Test database is filled.');
+          done();
+        });
+      });
+    });
   });
 
-  it('HTTP GET / - ping', function(done) {
+  it('HTTP GET /ping - ping REST API', function(done) {
     request(app)
       .get('/ping')
       .expect('Content-Type', 'text/plain; charset=utf-8')
@@ -25,4 +41,17 @@ describe('Athlete routes', function()  {
         done();
       });
   });
+
+  // it('HTTP POST /login - login with username and password', function(done) {
+  //   request(app)
+  //     .post('/login')
+  //     .expect('Content-Type', /json/)
+  //     .expect(200)
+  //     .end(function(err, res) {
+  //       if (err) throw err;
+  //
+  //       expect(res.text).toBe('TODO');
+  //       done();
+  //     });
+  // });
 });
